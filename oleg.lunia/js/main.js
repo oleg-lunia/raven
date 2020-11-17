523a11ecc0784050c4d6a8d3736793a9d1b83520
oleg.lunia <oleglunja2@gmail.com>
2020-11-17 22:42:33 +0300

    Initial commit.

--- oleg.lunia/js/main.js
+++ oleg.lunia/js/main.js
@@ -2,3 +2,32 @@ $('.slider').slick({
     infinite: true,
     slidesToShow:1,
     slidesToScroll:1})
+
+
+document.querySelector('.menu-burger__header').addEventListener('click', function(){
+    document.querySelector('.menu-burger__header').classList.toggle('open-menu');
+    document.querySelector('.header-nav').classList.toggle("open-menu");
+})
+
+
+
+document.querySelector('.menu-burger__header').addEventListener('click', function(event){
+    document.querySelector('.menu-burger__header').classList.toggle('active');
+    document.querySelector('body').classList.toggle("fixed-page");
+})
+
+
+// (function() {
+//     'use strict';
+    
+//     function backToTop() {
+//         if (window.pageYOffset > 0) {
+//             window.scrollBy(0, -80);
+//             setTimeout(backToTop, 0);
+//         }
+//     }
+    
+//     var goTopBtn = document.querySelector('.back_to_top');
+    
+//     goTopBtn.addEventListener('click', backToTop);
+// })();
